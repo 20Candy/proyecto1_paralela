845d2057ed3f233c44dd6886238a7b53381d8f9c
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2023-08-18 23:45:38 -0600

    fix

--- secuencia.cpp
+++ secuencia.cpp
@@ -27,12 +27,31 @@ struct Particle {
 
 std::vector<Particle> particles;
 
+std::chrono::high_resolution_clock::time_point previousFrameTime;
+int frameCount = 0;
+float fps = 0.0f;
+
 void DrawParticles() {
     glClear(GL_COLOR_BUFFER_BIT);
 
+    frameCount++;
+    std::chrono::high_resolution_clock::time_point currentFrameTime = std::chrono::high_resolution_clock::now();
+    float deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentFrameTime - previousFrameTime).count() / 1000.0f;
+    if (deltaTime >= 1.0f) {
+        fps = static_cast<float>(frameCount) / deltaTime;
+        frameCount = 0;
+        previousFrameTime = currentFrameTime;
+    }
+
+    glColor3f(1.0f, 1.0f, 1.0f);
+    glRasterPos2f(-WINDOW_WIDTH / 2 + 10, -WINDOW_HEIGHT / 2 + 10);
+    std::string fpsText = "FPS: " + std::to_string(static_cast<int>(fps));
+    for (char c : fpsText) {
+        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, c);
+    }
+
     for (const Particle& particle : particles) {
         glColor3f(particle.colorR, particle.colorG, particle.colorB);
-
         glBegin(GL_TRIANGLE_FAN);
         glVertex2f(particle.posX, particle.posY);
         const int numSegments = 16;
@@ -45,50 +64,24 @@ void DrawParticles() {
         glEnd();
     }
 
-    // Calculate FPS
-    frameCount++;
-    std::chrono::high_resolution_clock::time_point currentFrameTime = std::chrono::high_resolution_clock::now();
-    float deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentFrameTime - previousFrameTime).count() / 1000.0f;
-    if (deltaTime >= 1.0f) {
-        fps = static_cast<float>(frameCount) / deltaTime;
-        frameCount = 0;
-        previousFrameTime = currentFrameTime;
-    }
-
-    // Display FPS
-    glColor3f(1.0f, 1.0f, 1.0f);
-    glRasterPos2f(-WINDOW_WIDTH / 2 + 10, -WINDOW_HEIGHT / 2 + 10);
-    std::string fpsText = "FPS: " + std::to_string(static_cast<int>(fps));
-    for (char c : fpsText) {
-        glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, c);
-    }
-
     glutSwapBuffers();
 }
-
 
 void UpdateParticles(int value) {
     for (size_t i = 0; i < particles.size(); i++) {
         particles[i].posX += particles[i].velocityX;
         particles[i].posY += particles[i].velocityY;
-
-        // Bounce off the window edges
         if (particles[i].posX < -WINDOW_WIDTH / 2 + PARTICLE_RADIUS || particles[i].posX > WINDOW_WIDTH / 2 - PARTICLE_RADIUS) {
             particles[i].velocityX = -particles[i].velocityX;
         }
-
         if (particles[i].posY < -WINDOW_HEIGHT / 2 + PARTICLE_RADIUS || particles[i].posY > WINDOW_HEIGHT / 2 - PARTICLE_RADIUS) {
             particles[i].velocityY = -particles[i].velocityY;
         }
-
-        // Check for collisions with other particles
         for (size_t j = i + 1; j < particles.size(); j++) {
             float dx = particles[j].posX - particles[i].posX;
             float dy = particles[j].posY - particles[i].posY;
             float distance = std::sqrt(dx * dx + dy * dy);
-
             if (distance < PARTICLE_RADIUS * 2) {
-                // Collide with the other particle and update velocities
                 particles[i].velocityX = -particles[i].velocityX;
                 particles[i].velocityY = -particles[i].velocityY;
                 particles[j].velocityX = -particles[j].velocityX;
@@ -96,9 +89,8 @@ void UpdateParticles(int value) {
             }
         }
     }
-
     glutPostRedisplay();
-    glutTimerFunc(16, UpdateParticles, 0); 
+    glutTimerFunc(16, UpdateParticles, 0);
 }
 
 int main(int argc, char** argv) {
@@ -106,18 +98,14 @@ int main(int argc, char** argv) {
         std::cout << "Usage: " << argv[0] << " numParticles\n";
         return 1;
     }
-
     previousFrameTime = std::chrono::high_resolution_clock::now();
-
     int numParticles = std::atoi(argv[1]);
-
     std::random_device rd;
     std::default_random_engine generator(rd());
     std::uniform_real_distribution<float> randomFloatX(-WINDOW_WIDTH / 2 + PARTICLE_RADIUS, WINDOW_WIDTH / 2 - PARTICLE_RADIUS);
     std::uniform_real_distribution<float> randomFloatY(-WINDOW_HEIGHT / 2 + PARTICLE_RADIUS, WINDOW_HEIGHT / 2 - PARTICLE_RADIUS);
     std::uniform_real_distribution<float> randomVelocity(-10.0f, 10.0f);
     std::uniform_real_distribution<float> randomColor(0.0f, 1.0f);
-
     for (int i = 0; i < numParticles; i++) {
         float vx = randomVelocity(generator);
         float vy = randomVelocity(generator);
@@ -128,25 +116,18 @@ int main(int argc, char** argv) {
         float b = randomColor(generator);
         particles.emplace_back(vx, vy, x, y, r, g, b);
     }
-
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
     glutInitWindowSize(WINDOW_WIDTH, WINDOW_HEIGHT);
     glutCreateWindow("Particle Animation");
-
     glMatrixMode(GL_PROJECTION);
     glLoadIdentity();
     glOrtho(-WINDOW_WIDTH / 2, WINDOW_WIDTH / 2, -WINDOW_HEIGHT / 2, WINDOW_HEIGHT / 2, -1.0, 1.0);
-
     glMatrixMode(GL_MODELVIEW);
     glLoadIdentity();
-
     glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
-
     glutDisplayFunc(DrawParticles);
     glutTimerFunc(0, UpdateParticles, 0);
-
     glutMainLoop();
-
     return 0;
 }
