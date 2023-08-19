32e8239f7ffed95462f78597f335e6c001a804a5
Carol Arevalo <60670458+20Candy@users.noreply.github.com>
2023-08-18 23:16:28 -0600

    wip

--- secuencia.cpp
+++ secuencia.cpp
@@ -12,14 +12,13 @@ const float MAX_DISTANCE = 500.0f;
 
 struct Particle {
     bool isAlpha;
-    int colorID;
     float velocityX;
     float velocityY;
     float posX;
     float posY;
 
-    Particle(bool alpha, int color, float vx, float vy, float x, float y)
-        : isAlpha(alpha), colorID(color), velocityX(vx), velocityY(vy), posX(x), posY(y) {}
+    Particle(bool alpha, float vx, float vy, float x, float y)
+        : isAlpha(alpha), velocityX(vx), velocityY(vy), posX(x), posY(y) {}
 };
 
 std::vector<Particle> particles;
@@ -29,15 +28,9 @@ void DrawParticles() {
 
     for (const Particle& particle : particles) {
         if (particle.isAlpha)
-            glColor3f(0.0f, 0.0f, 1.0f);
-        else if (particle.colorID == 1)
-            glColor3f(1.0f, 0.0f, 0.0f);
-        else if (particle.colorID == 2)
-            glColor3f(0.0f, 1.0f, 0.0f);
-        else if (particle.colorID == 3)
-            glColor3f(1.0f, 1.0f, 0.0f);
-        else if (particle.colorID == 4)
-            glColor3f(1.0f, 0.5f, 0.0f);
+            glColor3f(0.0f, 0.0f, 1.0f); // Blue for alpha particles
+        else
+            glColor3f(1.0f, 0.0f, 0.0f); // Red for beta particles
 
         glBegin(GL_TRIANGLE_FAN);
         glVertex2f(particle.posX, particle.posY);
@@ -55,16 +48,16 @@ void DrawParticles() {
 }
 
 void UpdateParticles(int value) {
-    for (size_t i = 0; i < particles.size(); i++) {
-        if (!particles[i].isAlpha) {
-            int closestAlphaIndex = -1;
+    for (Particle& particle : particles) {
+        if (!particle.isAlpha) {
             float minDistance = MAX_DISTANCE;
+            int closestAlphaIndex = -1;
 
             for (size_t j = 0; j < particles.size(); j++) {
                 if (particles[j].isAlpha && j != i) {
                     float distance = std::sqrt(
-                        std::pow(particles[j].posX - particles[i].posX, 2) +
-                        std::pow(particles[j].posY - particles[i].posY, 2)
+                        std::pow(particles[j].posX - particle.posX, 2) +
+                        std::pow(particles[j].posY - particle.posY, 2)
                     );
 
                     if (distance < minDistance) {
@@ -74,50 +67,26 @@ void UpdateParticles(int value) {
                 }
             }
 
-            if (closestAlphaIndex != -1 && minDistance < MAX_DISTANCE) {
-                float dx = particles[i].posX - particles[closestAlphaIndex].posX;
-                float dy = particles[i].posY - particles[closestAlphaIndex].posY;
-                float distance = std::max(minDistance, 0.5f);
-                particles[i].velocityX -= (dx * 100) / distance;
-                particles[i].velocityY -= (dy * 100) / distance;
-                particles[i].velocityX *= FRICTION;
-                particles[i].velocityY *= FRICTION;
+            if (closestAlphaIndex != -1) {
+                const float dx = particle.posX - particles[closestAlphaIndex].posX;
+                const float dy = particle.posY - particles[closestAlphaIndex].posY;
+                const float distance = std::max(minDistance, 0.5f);
+                particle.velocityX -= (dx * 100) / distance;
+                particle.velocityY -= (dy * 100) / distance;
+                particle.velocityX *= FRICTION;
+                particle.velocityY *= FRICTION;
             }
         }
 
-        particles[i].posX += particles[i].velocityX;
-        particles[i].posY += particles[i].velocityY;
+        particle.posX += particle.velocityX;
+        particle.posY += particle.velocityY;
 
-        if (particles[i].posX < -WINDOW_WIDTH / 2 + PARTICLE_RADIUS || particles[i].posX > WINDOW_WIDTH / 2 - PARTICLE_RADIUS) {
-            particles[i].velocityX = -particles[i].velocityX;
+        if (particle.posX < -WINDOW_WIDTH / 2 + PARTICLE_RADIUS || particle.posX > WINDOW_WIDTH / 2 - PARTICLE_RADIUS) {
+            particle.velocityX = -particle.velocityX;
         }
 
-        if (particles[i].posY < -WINDOW_HEIGHT / 2 + PARTICLE_RADIUS || particles[i].posY > WINDOW_HEIGHT / 2 - PARTICLE_RADIUS) {
-            particles[i].velocityY = -particles[i].velocityY;
-        }
-
-        for (size_t j = 0; j < particles.size(); j++) {
-            if (j != i) {
-                float dx = particles[j].posX - particles[i].posX;
-                float dy = particles[j].posY - particles[i].posY;
-                float distance = std::sqrt(dx * dx + dy * dy);
-
-                if (distance < PARTICLE_RADIUS * 2.0f) {
-                    float overlap = PARTICLE_RADIUS * 2.0f - distance;
-                    float adjustX = (overlap * dx) / distance;
-                    float adjustY = (overlap * dy) / distance;
-
-                    particles[i].posX -= adjustX * 0.5f;
-                    particles[i].posY -= adjustY * 0.5f;
-                    particles[j].posX += adjustX * 0.5f;
-                    particles[j].posY += adjustY * 0.5f;
-
-                    particles[i].velocityX *= -1.0f;
-                    particles[i].velocityY *= -1.0f;
-                    particles[j].velocityX *= -1.0f;
-                    particles[j].velocityY *= -1.0f;
-                }
-            }
+        if (particle.posY < -WINDOW_HEIGHT / 2 + PARTICLE_RADIUS || particle.posY > WINDOW_HEIGHT / 2 - PARTICLE_RADIUS) {
+            particle.velocityY = -particle.velocityY;
         }
     }
 
@@ -125,32 +94,31 @@ void UpdateParticles(int value) {
     glutTimerFunc(0, UpdateParticles, 0);
 }
 
-
 int main(int argc, char** argv) {
-    if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " numberOfParticles\n";
+    if (argc < 3) {
+        std::cout << "Usage: " << argv[0] << " numberOfAlphaParticles numberOfBetaParticles\n";
         return 1;
     }
 
-    int numParticles = std::atoi(argv[1]);
+    int numAlphaParticles = std::atoi(argv[1]);
+    int numBetaParticles = std::atoi(argv[2]);
 
     std::random_device rd;
     std::default_random_engine generator(rd());
     std::uniform_real_distribution<float> randomFloat(-1.0f, 1.0f);
 
-    for (int i = 0; i < numParticles; i++) {
-        bool isAlpha = (i % 2 == 0);
-        float vx = 0.02f * randomFloat(generator); // Adjust the factor to control speed
-        float vy = 0.02f * randomFloat(generator); // Adjust the factor to control speed
-        float x = randomFloat(generator) * (WINDOW_WIDTH / 2 - PARTICLE_RADIUS);
-        float y = randomFloat(generator) * (WINDOW_HEIGHT / 2 - PARTICLE_RADIUS);
-        particles.emplace_back(isAlpha, vx, vy, x, y);
+    for (int i = 0; i < numAlphaParticles; i++) {
+        float vx = randomFloat(generator) * 10.0f;
+        float vy = randomFloat(generator) * 10.0f;
+        float x = randomFloat(generator) * 700.0f;
+        float y = randomFloat(generator) * 400.0f;
+        particles.emplace_back(true, vx, vy, x, y);
     }
 
     for (int i = 0; i < numBetaParticles; i++) {
         float x = randomFloat(generator) * 700.0f;
         float y = randomFloat(generator) * 400.0f;
-        particles.emplace_back(false, i % 4 + 1, 0.0f, 0.0f, x, y);
+        particles.emplace_back(false, 0.0f, 0.0f, x, y);
     }
 
     glutInit(&argc, argv);
