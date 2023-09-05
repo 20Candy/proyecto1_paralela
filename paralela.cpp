897250cecfc0357f4f861a76f1d9b57f3ff337c9
Roberto-VC <60154582+Roberto-VC@users.noreply.github.com>
2023-09-04 18:51:03 -0600

    Comment and Defense paralela.cpp

--- paralela.cpp
+++ paralela.cpp
@@ -6,10 +6,12 @@
 #include <chrono>
 #include <random>
 #include <omp.h>
+#include <string>
+#include <cstdlib> // Librerías que importar
 
 const int WINDOW_WIDTH = 1920;
-const int WINDOW_HEIGHT = 1080;
-const float PARTICLE_RADIUS = 60.0f;
+const int WINDOW_HEIGHT = 1080; //Tamaño de la pantalla.
+const float PARTICLE_RADIUS = 60.0f; //Tamaño de las partículas.
 
 struct Particle {
     float velocityX;
@@ -24,26 +26,34 @@ struct Particle {
 
     Particle(float vx, float vy, float x, float y, float r, float g, float b, float ch, float rad)
         : velocityX(vx), velocityY(vy), posX(x), posY(y), colorR(r), colorG(g), colorB(b), color_change(ch), radius(rad) {}
-};
+}; //Estructura para simular una particula. 
 
-std::vector<Particle> particles;
+std::vector<Particle> particles; //Vector para partículas.
 
 
 std::chrono::high_resolution_clock::time_point previousFrameTime;
 int frameCount = 0;
-float fps = 0.0f;
+float fps = 0.0f; // Implementación de FPS.
 
 int numParticlesToCreate = 0;
-bool creationFinished = false;
+bool creationFinished = false; //Cantidad de partículas y creación respectiva
 
+
+//CreateParticle
+//0 Parametros
+//Crear Partículas
+//Retorno nulo
 void CreateParticle() {
     // Tomar el tiempo de inicio
     std::chrono::high_resolution_clock::time_point startTime = std::chrono::high_resolution_clock::now();
 
     #pragma omp parallel
-    {
+    {  
+        //Creación paralela de las particulas
         std::random_device rd;
         std::mt19937 generator(rd());
+
+       //Distribución aleatoria para crear las particulas basada en radio, color, posición y movimiento
 
         std::uniform_real_distribution<float> randomRadius(20.0f, PARTICLE_RADIUS);
         std::uniform_real_distribution<float> randomFloatX(-WINDOW_WIDTH / 2 + PARTICLE_RADIUS, WINDOW_WIDTH / 2 - PARTICLE_RADIUS);
@@ -63,10 +73,11 @@ void CreateParticle() {
             float b = randomColor(generator);
 
             particles[i] = Particle(vx, vy, x, y, r, g, b, 0.0f, radius);
-        }
+        }//Creación respectivo en un Parallel For.
 
         #pragma omp master
-        {
+        {   
+            //Master que cuenta el tiempo de ejecución.
             std::chrono::high_resolution_clock::time_point endTime = std::chrono::high_resolution_clock::now();
             std::chrono::duration<double> totalTime = std::chrono::duration_cast<std::chrono::duration<double>>(endTime - startTime);
             std::cout << "Tiempo de creación de partículas: " << totalTime.count() << " segundos\n";
@@ -74,17 +85,20 @@ void CreateParticle() {
     } 
 
 }
-
+//DrawParticulas
+//0 Parametros
+//Dibuja la particulas en la pantalla 
+//Retorno nulo
 void DrawParticles() {
     glClear(GL_COLOR_BUFFER_BIT);
 
     for (size_t i = 0; i < numParticlesToCreate; i++) {
-        // Dibujar el cuerpo (un círculo grande)
+        // Dibujar el cuerpo (un círculo grande). Se empiezan a crear.
         glColor3f(particles[i].colorR, particles[i].colorG, particles[i].colorB);
         glBegin(GL_TRIANGLE_FAN);
         glVertex2f(particles[i].posX, particles[i].posY);
         const int numSegments = 64;
-        
+        //Angulo de movimiento basado en su posición y movimiento        
         for (int j = 0; j <= numSegments; j++) {
             float angle = j * 2.0f * M_PI / numSegments;
             float dx = particles[i].radius * std::cos(angle);
@@ -101,6 +115,7 @@ void DrawParticles() {
     for (char c : fpsText) {
         glutBitmapCharacter(GLUT_BITMAP_HELVETICA_12, c);
     }
+   //Se tiene el bitmap, y empieza a colorear en la pantalla.
 
     frameCount++;
     std::chrono::high_resolution_clock::time_point currentFrameTime = std::chrono::high_resolution_clock::now();
@@ -110,14 +125,20 @@ void DrawParticles() {
         frameCount = 0;
         previousFrameTime = currentFrameTime;
     }
+    //Se tiene el framerate, haciendo la cantidad de frames divido por el tiempo pasado. 
 
     glutSwapBuffers();
+    //Cambio de Buffers
 }
-
+//UpdateParticle
+//0 Parametros
+//Actualiza la particulas a dibujar.
+//Retorno nulo
 void UpdateParticles(int value) {
 
     std::chrono::high_resolution_clock::time_point currentFrameTime = std::chrono::high_resolution_clock::now();
     float deltaTime = std::chrono::duration_cast<std::chrono::milliseconds>(currentFrameTime - previousFrameTime).count() / 1000.0f;
+    //Cambio para los tiempos y el framerate. 
 
     #pragma omp parallel
     {
@@ -126,12 +147,14 @@ void UpdateParticles(int value) {
 
         std::uniform_real_distribution<float> randomColor(0.0f, 1.0f);
         std::uniform_int_distribution<int> randomInt(5, 10);
+        //Distribución aleatorio para los datos.
 
         #pragma omp for
+        //Paralelización de los datos para los circulos. Hace el cambio de posición basado en su belocidad.
         for (int i = 0; i < numParticlesToCreate; i++) {
             particles[i].posX += particles[i].velocityX;
             particles[i].posY += particles[i].velocityY;
-
+  
             particles[i].color_change += deltaTime;
             if (particles[i].color_change >= randomInt(generator)) { // Change color every random seconds
                 particles[i].colorR = randomColor(generator);
@@ -145,7 +168,7 @@ void UpdateParticles(int value) {
             }
             if (particles[i].posY < -WINDOW_HEIGHT / 2 + particles[i].radius || particles[i].posY > WINDOW_HEIGHT / 2 - particles[i].radius) {
                 particles[i].velocityY = -particles[i].velocityY;
-            }
+            } //La nueva posición en el caso que si se topa en la pantalla.
 
             
             // Cambiar el color de la partícula según el promedio de los colores de las partículas vecinas
@@ -156,6 +179,7 @@ void UpdateParticles(int value) {
             {
                 float privateAvgColorR = 0.0f, privateAvgColorG = 0.0f, privateAvgColorB = 0.0f;
                 int privateNeighborCount = 0;
+                //Otra paralelización para cambiar los colores basado en posición y sus vecionas.
                 
                 #pragma omp for
                 for (size_t j = 0; j < numParticlesToCreate; j++) {
@@ -177,6 +201,7 @@ void UpdateParticles(int value) {
                     avgColorG += privateAvgColorG;
                     avgColorB += privateAvgColorB;
                     neighborCount += privateNeighborCount;
+                    //Sección crítica para que solo lo haga un thread, los colores vecinos.
                 }
             }
 
@@ -194,13 +219,40 @@ void UpdateParticles(int value) {
     }
     glutPostRedisplay();
     glutTimerFunc(16, UpdateParticles, 0);
+    //Hace un nuevo display y cambia la función de tiempo
 }
 
 int main(int argc, char** argv) {
     if (argc < 2) {
-        std::cout << "Usage: " << argv[0] << " numParticles\n";
+        std::cout << "Usage: " << argv[0] << " numParticles\n";  //Se asegura que el usuario ingrese un input.
         return 1;
     }
+
+    bool isNumber = false;
+    for (char c : input) {
+        if (!std::isdigit(c)) {
+            isNumber = true;
+            break;
+        }
+    }
+
+    if (isNumber) {
+        // Seguridad. Asegura que sea un entero positivo, menor de 15000 para velocidad
+        std::cout << "Debe de ingresar un número, no String.\n";
+        return 1; //Programación Defensiva 
+    } else {
+         int number = std::stoi(input);
+         if (numner > 15000){
+             std::cout << "Debe de ingresar un número mayor que 15000.\n";
+             return 1;
+         }
+        if (number < 0){
+            std::cout << "Debe de ingresar un número positvo.\n";
+            return 1;
+        }
+            
+    }
+    
 
     omp_set_num_threads(4);                             // Establece el número de hilos a utilizar
 
